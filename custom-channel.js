76da4fb0fba4a1560d7b65f4dc275d3e8ac39b64
Afan Ahmad Khan <afan.khan50@gmail.com>
2016-05-17 09:29:51 -0400

    Removal of duplicated code, addition of setup.js and code refactoring of custom-channel.js

--- custom-channel.js
+++ custom-channel.js
@@ -1,37 +1,14 @@
 // hiding all the data divs initially
-$('#automatedDataContent').hide();
-$('#mostRecentData').hide();
-$('.validCustomPanel').hide();
-$('.errorCustomPanel').hide();
-$('#channelinfomanual').hide();
-$('#channelinfoautomated').hide();
-$('#channelinfogetdata').hide();
-$("#error-message").hide();
+hideContent();
 
 // setting up the table headers for the automated data display
-var automatedHeaderContent = '<tr>';
-var $automatedDataContent = $("#automatedDataContent");
-automatedHeaderContent += "<th>Entry Id</th><th>Field Value</th>";
-$automatedDataContent.append(automatedHeaderContent);
+automatedDataTable();
 
 // call to insert data to the channel every 5 seconds
 channelData = setInterval('putDataIntoChannel', 5000);
 
-
-//set up thingspeak array
-var thingspeak = {};
-thingspeak['url'] = 'https://api.thingspeak.com';
-thingspeak['channel'] = 114928;
-thingspeak['read_api_key'] = '36K19EKNTI4Q3LS7';
-thingspeak['write_api_key'] = '7BHR9380R6UZSUDZ';
-thingspeak['results'] = 5;
-thingspeak['field_number'] = 1;
-
 // setting up the default values for the modal
-$('#modalChannelId').val(thingspeak['channel']);
-$('#modalReadKey').val(thingspeak['read_api_key']);
-$('#modalWriteKey').val(thingspeak['write_api_key']);
-$('#modalfieldNumber').val(thingspeak['field_number']);
+settingUpModalContent();
 
 /* code for modal starts */
 $("#modalChannelInfo").click(function() {
@@ -40,29 +17,9 @@ $("#modalChannelInfo").click(function() {
     thingspeak['read_api_key'] = document.getElementById('modalReadKey').value;
     thingspeak['write_api_key'] = document.getElementById('modalWriteKey').value;
     thingspeak['field_number'] = document.getElementById('modalfieldNumber').value;
-    //console.log(thingspeak);
-    // Re-setting the modal params to channel params
-    $('#modalChannelId').val(thingspeak['channel']);
-    $('#modalReadKey').val(thingspeak['read_api_key']);
-    $('#modalWriteKey').val(thingspeak['write_api_key']);
-    $('#modalfieldNumber').val(thingspeak['field_number']);
-
-    // hiding the divs on change of channel settings
-    $('#automatedDataContent').hide();
-    $('#mostRecentData').hide();
-    $('.validCustomPanel').hide();
-    $('.errorCustomPanel').hide();
-    $('#channelinfomanual').hide();
-    $('#channelinfoautomated').hide();
-    $('#channelinfogetdata').hide();
-    $("#error-message").hide();
-
-    // Resetting the automated data content on change of channel params
-    $('#automatedDataContent').empty();
-    var automatedHeaderContent = '<tr>';
-    var $automatedDataContent = $("#automatedDataContent");
-    automatedHeaderContent += "<th>Entry Id</th><th>Field Value</th>";
-    $automatedDataContent.append(automatedHeaderContent);
+    settingUpModalContent();
+    hideContent();
+    automatedDataContent();
     // Resetting the play button on change of channel params
     if ($('#start').find(".glyphicon").hasClass("glyphicon-pause")) {
         // show play button state
@@ -71,6 +28,95 @@ $("#modalChannelInfo").click(function() {
     }
 });
 /* code for modal ends */
+
+/* code for putting data manually into the channel starts */
+$("#manualInputButton").click(function(event) {
+    event.preventDefault();
+    if (document.getElementById('manualInputVal').value === "") {
+        $("#error-message").slideDown(function() {
+            setTimeout(function() {
+                $("#error-message").slideUp();
+            }, 1500);
+        });
+    } else {
+        $('#channelinfomanual').show();
+        $.getJSON(thingspeak['url'] + '/channels/' + $('#modalChannelId').val() + '/fields/' + $('#modalfieldNumber').val() + '.json?results=' + thingspeak['results'] +
+            '&location=' + thingspeak['location'] + '&status=' + thingspeak['status'] + '&api_key=' + $('#modalReadKey').val(),
+            function(data) {
+                // create table and headers if channel object exists
+                if (data) {
+                    console.log(data);
+                    $('#channelidmanual').html('<b>' + data.channel.id + '</b>');
+                    $('#channelnamemanual').html('<b>' + data.channel.name + '</b>');
+                    var fNumber = $('#modalfieldNumber').val();
+                    setFieldLabel(fNumber, data, '#fieldnamemanual');
+                }
+            });
+        $.ajax({
+            url: 'https://api.thingspeak.com/update?api_key=' + $('#modalWriteKey').val() + '&field' + $('#modalfieldNumber').val() + '=' + document.getElementById('manualInputVal').value,
+            dataType: "text",
+            success: function(data) {
+                if (data != 0) {
+                    $('#customMainContent').empty();
+                    $('.validCustomPanel').show();
+                    $('.errorCustomPanel').hide();
+                    var $customMainContent = $("#customMainContent");
+                    $customMainContent.append('<b>Entry Id:  </b>' + data + '<br>');
+                    $customMainContent.append('<b>Value:  </b>' + document.getElementById('manualInputVal').value);
+                    document.getElementById('manualInputVal').value = null;
+                } else {
+                    $('#customErrorContent').empty();
+                    $('.errorCustomPanel').show();
+                    $('.validCustomPanel').hide();
+                    var $customErrorContent = $("#customErrorContent");
+                    $customErrorContent.append(data);
+                    document.getElementById('manualInputVal').value = null;
+                }
+            },
+            error: function() {}
+        });
+    }
+});
+/* putting data manually into the channel ends */
+
+/* code for putting data every 15 seconds into the channel starts */
+function putDataIntoChannel() {
+        $('#channelinfoautomated').show();
+        $('#automatedDataContent').show();
+        $('#errorContent').show();
+        var tableContent = '<tr>';
+        var inputData = Math.floor((Math.random() * 1000) + 1);
+
+        $.getJSON(thingspeak['url'] + '/channels/' + $('#modalChannelId').val() + '/fields/' + $('#modalfieldNumber').val() + '.json?results=' + thingspeak['results'] + '&location=' +
+            thingspeak['location'] + '&status=' + thingspeak['status'] + '&api_key=' + $('#modalReadKey').val(),
+            function(data) {
+                // create table and headers if channel object exists
+                if (data) {
+                    //console.log(data);
+                    $('#channelidautomated').html('<b>' + data.channel.id + '</b>');
+                    $('#channelnameautomated').html('<b>' + data.channel.name + '</b>');
+                    // setting the correct field label
+                    var fNumber = $('#modalfieldNumber').val();
+                    setFieldLabel(fNumber, data, '#fieldnameautomated');
+
+                }
+            });
+        $.ajax({
+            url: 'https://api.thingspeak.com/update?api_key=' + $('#modalWriteKey').val() + '&field' + $('#modalfieldNumber').val() + '=' + inputData,
+            dataType: "text",
+            success: function(data) {
+                if (data != 0) {
+                    var $automatedDataContent = $("#automatedDataContent");
+                    tableContent += "<td>" + data + "</td>" + "<td>" + inputData + "</td>";
+                    $automatedDataContent.append(tableContent);
+                }
+            },
+            error: function() {
+                alert('failure');
+            }
+        });
+    }
+    /* code for putting data every 15 seconds into the channel ends */
 
 /* code for getting top 5 data starts */
 $("#getData").click(function() {
@@ -81,14 +127,12 @@ $("#getData").click(function() {
     var $mostRecentData = $("#mostRecentData");
     headerContent += "<th>Created</th><th>Entry Id</th><th>Field value</th>";
     $mostRecentData.append(headerContent);
-
     // get the data with a webservice call
     $.getJSON(thingspeak['url'] + '/channels/' + $('#modalChannelId').val() + '/fields/' + $('#modalfieldNumber').val() + '.json?results=' + thingspeak['results'] +
         '&location=' + thingspeak['location'] + '&status=' + thingspeak['status'] + '&api_key=' + $('#modalReadKey').val(),
         function(data) {
             // create table and headers if channel object exists
             if (data) {
-                console.log(data);
                 $('#channelidgetdata').html('<b>' + data.channel.id + '</b>');
                 $('#channelnamegetdata').html('<b>' + data.channel.name + '</b>');
             }
@@ -97,7 +141,6 @@ $("#getData").click(function() {
                     var tableContent = '<tr>';
                     var fNumber = $('#modalfieldNumber').val();
                     var $mostRecentData = $("#mostRecentData");
-
                     tableContent += "<td><time class='timeago' datetime='" + feed.created_at + "'>" + feed.created_at + "</time></td>" + "<td>" + feed.entry_id + "</td>";
                     // setting the correct field label
                     if (fNumber == 1) {
@@ -142,140 +185,33 @@ $("#getData").click(function() {
 });
 /* code for getting top 5 data ends */
 
-/* code for putting data every 15 seconds into the channel starts */
-function putDataIntoChannel() {
-    $('#channelinfoautomated').show();
-    $('#automatedDataContent').show();
-    $('#errorContent').show();
-    var tableContent = '<tr>';
-    var inputData = Math.floor((Math.random() * 1000) + 1);
-    
-    $.getJSON(thingspeak['url'] + '/channels/' + $('#modalChannelId').val() + '/fields/' + $('#modalfieldNumber').val() + '.json?results=' + thingspeak['results'] + '&location=' +
-        thingspeak['location'] + '&status=' + thingspeak['status'] + '&api_key=' + $('#modalReadKey').val(),
-        function(data) {
-            // create table and headers if channel object exists
-            if (data) {
-                //console.log(data);
-                $('#channelidautomated').html('<b>' + data.channel.id + '</b>');
-                $('#channelnameautomated').html('<b>' + data.channel.name + '</b>');
-                // setting the correct field label
-                var fNumber = $('#modalfieldNumber').val();
-                if (fNumber == 1) {
-                    $('#fieldnameautomated').html('<b>' + data.channel.field1 + '</b>');
-                }
-                if (fNumber == 2) {
-                    $('#fieldnameautomated').html('<b>' + data.channel.field2 + '</b>');
-                }
-                if (fNumber == 3) {
-                    $('#fieldnameautomated').html('<b>' + data.channel.field3 + '</b>');
-                }
-                if (fNumber == 4) {
-                    $('#fieldnameautomated').html('<b>' + data.channel.field4 + '</b>');
-                }
-                if (fNumber == 5) {
-                    $('#fieldnameautomated').html('<b>' + data.channel.field5 + '</b>');
-                }
-                if (fNumber == 6) {
-                    $('#fieldnameautomated').html('<b>' + data.channel.field6 + '</b>');
-                }
-                if (fNumber == 7) {
-                    $('#fieldnameautomated').html('<b>' + data.channel.field7 + '</b>');
-                }
-                if (fNumber == 8) {
-                    $('#fieldnameautomated').html('<b>' + data.channel.field8 + '</b>');
-                }
-            }
-        });
-    $.ajax({
-        url: 'https://api.thingspeak.com/update?api_key=' + $('#modalWriteKey').val() + '&field' + $('#modalfieldNumber').val() + '=' + inputData,
-        dataType: "text",
-        success: function(data) {
-            if (data != 0) {
-                var $automatedDataContent = $("#automatedDataContent");
-                tableContent += "<td>" + data + "</td>" + "<td>" + inputData + "</td>";
-                $automatedDataContent.append(tableContent);
-            }
-        },
-        error: function() {
-            alert('failure');
-        }
-    });
-}
-/* code for putting data every 15 seconds into the channel ends */
-
-/* code for putting data manually into the channel starts */
-$("#manualInputButton").click(function(event) {
-    event.preventDefault();
-    if (document.getElementById('manualInputVal').value === "") {
-        $("#error-message").slideDown(function() {
-            setTimeout(function() {
-                $("#error-message").slideUp();
-            }, 1500);
-        });
-    } else {
-        $('#channelinfomanual').show();
-        $.getJSON(thingspeak['url'] + '/channels/' + $('#modalChannelId').val() + '/fields/' + $('#modalfieldNumber').val() + '.json?results=' + thingspeak['results'] +
-            '&location=' + thingspeak['location'] + '&status=' + thingspeak['status'] + '&api_key=' + $('#modalReadKey').val(),
-            function(data) {
-                // create table and headers if channel object exists
-                if (data) {
-                    console.log(data);
-                    $('#channelidmanual').html('<b>' + data.channel.id + '</b>');
-                    $('#channelnamemanual').html('<b>' + data.channel.name + '</b>');
-                    var fNumber = $('#modalfieldNumber').val();
-                    if (fNumber == 1) {
-                        $('#fieldnamemanual').html('<b>' + data.channel.field1 + '</b>');
-                    }
-                    if (fNumber == 2) {
-                        $('#fieldnamemanual').html('<b>' + data.channel.field2 + '</b>');
-                    }
-                    if (fNumber == 3) {
-                        $('#fieldnamemanual').html('<b>' + data.channel.field3 + '</b>');
-                    }
-                    if (fNumber == 4) {
-                        $('#fieldnamemanual').html('<b>' + data.channel.field4 + '</b>');
-                    }
-                    if (fNumber == 5) {
-                        $('#fieldnamemanual').html('<b>' + data.channel.field5 + '</b>');
-                    }
-                    if (fNumber == 6) {
-                        $('#fieldnamemanual').html('<b>' + data.channel.field6 + '</b>');
-                    }
-                    if (fNumber == 7) {
-                        $('#fieldnamemanual').html('<b>' + data.channel.field7 + '</b>');
-                    }
-                    if (fNumber == 8) {
-                        tableContent += "<td>" + feed.field8 + "</td>";
-                        $('#fieldnamemanual').html('<b>' + data.channel.field8 + '</b>');
-                    }
-                }
-            });
-        $.ajax({
-            url: 'https://api.thingspeak.com/update?api_key=' + $('#modalWriteKey').val() + '&field' + $('#modalfieldNumber').val() + '=' + document.getElementById('manualInputVal').value,
-            dataType: "text",
-            success: function(data) {
-                if (data != 0) {
-                    $('#customMainContent').empty();
-                    $('.validCustomPanel').show();
-                    $('.errorCustomPanel').hide();
-                    var $customMainContent = $("#customMainContent");
-                    $customMainContent.append('<b>Entry Id:  </b>' + data + '<br>');
-                    $customMainContent.append('<b>Value:  </b>' + document.getElementById('manualInputVal').value);
-                    document.getElementById('manualInputVal').value = null;
-                } else {
-                    $('#customErrorContent').empty();
-                    $('.errorCustomPanel').show();
-                    $('.validCustomPanel').hide();
-                    var $customErrorContent = $("#customErrorContent");
-                    $customErrorContent.append(data);
-                    document.getElementById('manualInputVal').value = null;
-                }
-            },
-            error: function() {}
-        });
+/* setting the field label for data insertion */
+function setFieldLabel(fNumber, data, fieldNamebox) {
+    if (fNumber == 1) {
+        $(fieldNamebox).html('<b>' + data.channel.field1 + '</b>');
     }
-});
-/* code for putting data manually into the channel starts */
+    if (fNumber == 2) {
+        $(fieldNamebox).html('<b>' + data.channel.field2 + '</b>');
+    }
+    if (fNumber == 3) {
+        $(fieldNamebox).html('<b>' + data.channel.field3 + '</b>');
+    }
+    if (fNumber == 4) {
+        $(fieldNamebox).html('<b>' + data.channel.field4 + '</b>');
+    }
+    if (fNumber == 5) {
+        $(fieldNamebox).html('<b>' + data.channel.field5 + '</b>');
+    }
+    if (fNumber == 6) {
+        $(fieldNamebox).html('<b>' + data.channel.field6 + '</b>');
+    }
+    if (fNumber == 7) {
+        $(fieldNamebox).html('<b>' + data.channel.field7 + '</b>');
+    }
+    if (fNumber == 8) {
+        $(fieldNamebox).html('<b>' + data.channel.field8 + '</b>');
+    }
+}
 
 // start / stop updates
 function playPause(button) {
@@ -297,7 +233,7 @@ function playPause(button) {
 }
 
 
-// code for flash message
+// flash message
 (function($) {
     $.fn.flash_message = function(options) {
         options = $.extend({
@@ -327,3 +263,33 @@ $('.btn-flash').click(function() {
         how: 'append'
     });
 });
+
+// hiding the divs
+function hideContent() {
+    $('#automatedDataContent').hide();
+    $('#mostRecentData').hide();
+    $('.validCustomPanel').hide();
+    $('.errorCustomPanel').hide();
+    $('#channelinfomanual').hide();
+    $('#channelinfoautomated').hide();
+    $('#channelinfogetdata').hide();
+    $("#error-message").hide();
+}
+
+// setting up header for the automated data entry table
+function automatedDataTable() {
+    $('#automatedDataContent').empty();
+    var automatedHeaderContent = '<tr>';
+    var $automatedDataContent = $("#automatedDataContent");
+    automatedHeaderContent += "<th>Entry Id</th><th>Field Value</th>";
+    $automatedDataContent.append(automatedHeaderContent);
+}
+
+
+// setting up modal content 
+function settingUpModalContent() {
+    $('#modalChannelId').val(thingspeak['channel']);
+    $('#modalReadKey').val(thingspeak['read_api_key']);
+    $('#modalWriteKey').val(thingspeak['write_api_key']);
+    $('#modalfieldNumber').val(thingspeak['field_number']);
+}
---
+++ setup.js
@@ -0,0 +1,8 @@
+//set up thingspeak array
+var thingspeak = {};
+thingspeak['url'] = 'https://api.thingspeak.com';
+thingspeak['channel'] = 114928;
+thingspeak['read_api_key'] = '36K19EKNTI4Q3LS7';
+thingspeak['write_api_key'] = '7BHR9380R6UZSUDZ';
+thingspeak['results'] = 5;
+thingspeak['field_number'] = 1;
