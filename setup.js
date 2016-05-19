09b46022b677a964852e73d2098f9b7dabbc5c61
Afan Ahmad Khan <afan.khan50@gmail.com>
2016-05-19 15:24:06 -0400

    Adding the channel number

--- setup.js
+++ setup.js
@@ -1,7 +1,7 @@
 //set up thingspeak array
 var thingspeak = {};
 thingspeak['url'] = 'https://api.thingspeak.com';
-thingspeak['channel'] = channel;
+thingspeak['channel'] = 114928;
 thingspeak['read_api_key'] = read_api_key;
 thingspeak['write_api_key'] = write_api_key;
 thingspeak['results'] = 5;
